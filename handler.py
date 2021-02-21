37323ed051e85487f57ad4583274306d8622f489
mohser sh <sh1361m@gmail.com>
2021-02-21 17:03:48 +0800

    fix

--- handler.py
+++ handler.py
@@ -7,37 +7,56 @@ import json
 
 
 
-model = ServerlessModel('./model', 'my-bert-models',
+model, tokenizer = ServerlessModel('./model', 'my-bert-models',
                         'qa/squad-distilbert.tar.gz')
 
 
 def predict_answer(event, context):
-    try:
-        print(event['body'])
-        body = json.loads(event['body'])
-        answer = model.predict(body['question'], body['context'])
+    print(event['body'])
+    body = json.loads(event['body'])
+    answer = model.predict(body['question'], body['context'])
 
-        return {
-            "statusCode": 200,
-            "headers": {
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*',
-                "Access-Control-Allow-Credentials": True
+    return {
+        "statusCode": 200,
+        "headers": {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            "Access-Control-Allow-Credentials": True
 
-            },
-            "body": json.dumps({'answer': answer})
-        }
-    except Exception as e:
-        print(repr(e))
-        return {
-            "statusCode": 500,
-            "headers": {
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*',
-                "Access-Control-Allow-Credentials": True
-            },
-            "body": json.dumps({"error": repr(e)})
-        }
+        },
+        "body": json.dumps({'answer': answer})
+    }
+
+
+
+
+# def predict_answer(event, context):
+#     try:
+#         print(event['body'])
+#         body = json.loads(event['body'])
+#         answer = model.predict(body['question'], body['context'])
+
+#         return {
+#             "statusCode": 200,
+#             "headers": {
+#                 'Content-Type': 'application/json',
+#                 'Access-Control-Allow-Origin': '*',
+#                 "Access-Control-Allow-Credentials": True
+
+#             },
+#             "body": json.dumps({'answer': answer})
+#         }
+#     except Exception as e:
+#         print(repr(e))
+#         return {
+#             "statusCode": 500,
+#             "headers": {
+#                 'Content-Type': 'application/json',
+#                 'Access-Control-Allow-Origin': '*',
+#                 "Access-Control-Allow-Credentials": True
+#             },
+#             "body": json.dumps({"error": repr(e)})
+#         }
 
 
 # event = {
